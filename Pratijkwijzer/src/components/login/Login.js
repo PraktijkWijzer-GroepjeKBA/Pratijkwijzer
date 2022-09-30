7288cd6ba0ada01a2ac94c1bd03b4f5fd1d3f9e7
AngelovanOsch1 <99641678+AngelovanOsch1@users.noreply.github.com>
2022-09-30 16:07:30 +0200

    Login + signup fe

--- Pratijkwijzer/src/components/login/Login.js
+++ Pratijkwijzer/src/components/login/Login.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './login.scss';
+import background from './mole-logo-image.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
   return (
     <div className='blur-background'>
-      <form>test</form>
+      <form className='login-form'>
+        <FontAwesomeIcon className='closing-form' icon={faXmark}></FontAwesomeIcon>
+      <div style={{ backgroundImage: `url(${background})` }} className='background-login-image'></div>
+        <div className='center-input'>
+        <h1>Login</h1>
+          <label>Email</label>
+        <input alt='#'></input>
+        <label>Password</label>
+        <input alt='#'></input>
+        <button>Submit</button>
+        <div className='login-info'>
+        <Link to='../Signup'><p>Don't have an account yet?</p></Link>
+        </div>
+        <div className='login-info'>
+        <p>Forgot password?</p>
+        </div>
+        </div>
+      </form>
       </div>
   )
 }
--- Pratijkwijzer/src/components/signup/Signup.js
+++ Pratijkwijzer/src/components/signup/Signup.js
@@ -1,8 +1,45 @@
 import React from 'react'
 
+import './signup.scss';
+import background from './mole-logo-image.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='blur-background'>
+      <form className='signup-form'>
+        <FontAwesomeIcon className='closing-form' icon={faXmark}></FontAwesomeIcon>
+      <div style={{ backgroundImage: `url(${background})` }} className='background-login-image'></div>
+        <div className='center-input'>
+          <h1>Make an account</h1>
+        <div className='input-field'>
+          <label>Email</label>
+          <input></input>
+        </div>
+        <div className='input-field'>
+          <label>Password</label>
+          <input></input>
+        </div>
+        <div className='input-field'>
+          <label>Repeat password</label>
+          <input></input>
+        </div>
+          <div className='input-container'>
+            <div className='field'>
+            <label>First name</label>
+            <input></input>
+            </div>
+            <div className='field'>
+            <label>Last name</label>
+            <input></input>
+            </div>
+          </div>
+          <button>Register</button>
+        </div>
+      </form>
+      </div>
   )
 }
 
