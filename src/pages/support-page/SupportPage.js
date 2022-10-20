a45f2cff2ebf9661892fbb1b91f2919746c09be2
MistrBrend <104073696+MistrBrend@users.noreply.github.com>
2022-10-20 14:33:29 +0200

    support page

---
+++ src/pages/support-page/SupportPage.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class SupportPage extends PureComponent {
+  render() {
+    return (
+      <div>SupportPage</div>
+    )
+  }
+}
+
+export default SupportPage
