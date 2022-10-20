5805ac08ebc414e82459bd74f1fbf4f7c353e839
MistrBrend <104073696+MistrBrend@users.noreply.github.com>
2022-10-20 15:57:42 +0200

    laatste push

--- src/components/header-support-page/headerSupportPage.js
+++ src/components/header-support-page/headerSupportPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './headerSupportPage.scss';
 
+import { Link } from 'react-router-dom';
+
 const HeaderSupport = () => {
   return (
-    <div>
-      header support page
-    </div>
+    <header>
+      <div className='supportNav'>
+          <div className='title'>CampingMolerat <span>/ Support</span></div>
+          <button>Logout</button>
+          <Link to='../Homepage'><button className='home-btn'>Home</button></Link>
+      </div>
+    </header>
   )
 }
 
