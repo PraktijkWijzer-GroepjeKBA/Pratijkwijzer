b9543ac8a37f86761f5526d3c89c51830f38cd60
MistrBrend <104073696+MistrBrend@users.noreply.github.com>
2022-11-10 15:34:40 +0100

    contact support

--- src/pages/support-page/SupportPage.js
+++ src/pages/support-page/SupportPage.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import HeaderSupportPage from '../../components/header-support-page/headerSupportPage';
+import './SupportPage.scss'
+import image from '../../img/home-page-image.png'
 
-// const SupportPage = () => {
-//   return (
-//     <div>
-//       <HeaderSupportPage />
+const SupportPage = () => {
+    return (
+        <div>
+            <HeaderSupportPage />
 
-//       <div> test test test test</div>
-//     </div>
-//   )
-// }
+            <div className='box'>
+                <div className='box__left-container'>
+                    <h2>Choose your problem</h2>
+                    <button className='button'>Repairs</button>
+                    <button className='button'>Cleaning</button>
+                    <button className='button'>Other</button>
+                </div>
+                <div className='box__right-container'>
+                    <div className='image-container-support'>
+                        <img src={image}></img>
+                    </div>
+                    <div className='information-container'>
+                        <h3>Telephone:  + 958305398</h3>
+                        <h3>Email: 433@gmail.com</h3>
+                        <h3>Location: Afrika, uganda</h3>
+                        <h3>Available times: Mo-Su 8:00-22:00</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-
-// login script {user ? <button className="logoutBtn" onClick={() => logout()}>Logout</button> : <Link to='../Login'><button className='login-btn'>Login</button></Link>}           
-          
-// export default SupportPage
+export default SupportPage
