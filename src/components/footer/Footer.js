847c2dba485a841e7f4b2981d9a8570aeae8124f
AngelovanOsch1 <99641678+AngelovanOsch1@users.noreply.github.com>
2023-09-04 20:14:54 +0200

    test message

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,43 +1,50 @@
-import React from 'react'
+import React from 'react';
 import '../footer/footer.scss';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-        <div className='footer-data-container'>
-            <div className='footer-column-container'>
-                <div className='footer-header'>Resources</div>
-                <div className='footer-text'>
-                    <a className='text-link' href='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                </div>
-            </div>
-            <div className='footer-column-container'>
-            <div className='footer-header'>Contacts</div>
-                <div className='footer-text'>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                </div>
-            </div>
-            <div className='footer-column-container'>
-            <div className='footer-header'>Links</div>
-                <div className='footer-text'>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                    <a className='text-link' ref='#'>Lorem ipsum</a>
-                </div>
-            </div>
+      <div className='footer-data-container'>
+        <div className='footer-column-container'>
+          <div className='footer-header'>Resources</div>
+          <div className='footer-text'>
+            <a className='text-link' href='#'>
+              Lorem ipsum
+            </a>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+          </div>
         </div>
-        <div className='footer-copyright-container'>
-            <p>Copyright Camping Molerat &copy; 2022</p>
+        <div className='footer-column-container'>
+          <div className='footer-header'>Links</div>
+          <div className='footer-text'>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+            <a className='text-link' ref='#'>
+              Lorem ipsum
+            </a>
+          </div>
         </div>
+      </div>
+      <div className='footer-copyright-container'>
+        <p>Copyright Camping Molerat &copy; 2022</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
